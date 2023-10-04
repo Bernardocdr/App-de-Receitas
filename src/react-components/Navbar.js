bfb2f695ce6e997d1ec008e6c65acf68a6bcf04f
Bernardocdr <bernardocdr@gmail.com>
2023-10-03 22:32:43 -0300

    Visual da sidebar foi atualizado

--- src/react-components/Navbar.js
+++ src/react-components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar(){
     return (
-        <div className="navbar">
-          <a href="#!"> F<span>oo</span>diesHub</a>
+        <div className="navbar container">
+          <a href="#!" className="logo"> <span>C</span>oo<span>kB</span>oo<span>k</span></a>
           <div className="nav-links">
-            <a href="#!">Início</a>
+            <a href="#!" className="active">Início</a>
             <a href="#!">Receitas</a>
             <a href="#!">Configurações</a>
             <a href="#!">Sobre</a>
